346fc97375251b9a98ea4b9b71b68e7eef55a104
uutanyang <uutan@qq.com>
2025-03-03 09:23:00 +0800

    常量设置

---
+++ internal/consts/app.go
@@ -0,0 +1,29 @@
+package consts
+
+const (
+	Running     = "Running"
+	UnHealthy   = "UnHealthy"
+	Error       = "Error"
+	Stopped     = "Stopped"
+	Installing  = "Installing"
+	DownloadErr = "DownloadErr"
+	Upgrading   = "Upgrading"
+	UpgradeErr  = "UpgradeErr"
+	Rebuilding  = "Rebuilding"
+	Syncing     = "Syncing"
+	SyncSuccess = "SyncSuccess"
+	Paused      = "Paused"
+	UpErr       = "UpErr"
+	SyncFailed  = "SyncFailed"
+
+	AppNormal   = "Normal"
+	AppTakeDown = "TakeDown"
+
+	AppResourceLocal  = "local"
+	AppResourceRemote = "remote"
+
+	CPUS          = "CPUS"
+	MemoryLimit   = "MEMORY_LIMIT"
+	HostIP        = "HOST_IP"
+	ContainerName = "CONTAINER_NAME"
+)
---
+++ internal/consts/status.go
@@ -0,0 +1,20 @@
+package consts
+
+const (
+	StatusRunning   = "Running"
+	StatusDone      = "Done"
+	StatusWaiting   = "Waiting"
+	StatusSuccess   = "Success"
+	StatusFailed    = "Failed"
+	StatusDeleted   = "Deleted"
+	StatusCanceled  = "Canceled"
+	StatusUploading = "Uploading"
+	StatusEnable    = "Enable"
+	StatusDisable   = "Disable"
+	StatusNone      = "None"
+	StatusOnRetry   = "OnRetry"
+	StatusLost      = "Lost"
+
+	OrderDesc = "descending"
+	OrderAsc  = "ascending"
+)
